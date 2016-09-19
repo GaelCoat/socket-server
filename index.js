519b7d7ab4c76f794dee15f825ec62375dc76105
gael coat <zoul.gaelcoat@gmail.com>
2016-09-19 14:02:32 +0200

    hope

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var port = process.env.PORT || 3000;
-var io = require('socket.io').listen(server);
-server.listen(port);
+'use strict';
 
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-io.sockets.on('connection', function(socket) {
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+const server = express()
+  .use((req, res) => res.send('hope') )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  console.log('Connected');
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
