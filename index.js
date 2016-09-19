8713eb4458db6e938ff7552c3ac184e5d8edcf5f
gael coat <zoul.gaelcoat@gmail.com>
2016-09-19 14:08:31 +0200

    hope

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const server = express()
 
 const io = socketIO(server);
 
+io.set('transports', ['xhr-polling']);
+io.set('polling duration', 10);
+
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
