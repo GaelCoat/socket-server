9e978d7621c11070d807dc54000815082226d925
gael coat <zoul.gaelcoat@gmail.com>
2016-09-19 13:53:35 +0200

    hope

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ var port = process.env.PORT || 3000;
 var io = require('socket.io').listen(server);
 server.listen(port);
 
-io.configure(function() {
-  io.set("transports", ["xhr-polling"])
-  io.set("polling duration", 10)
-});
 
 io.sockets.on('connection', function(socket) {
 
